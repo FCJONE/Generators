4d64281464ad33e12ed88e00c5eb883004252c87
Dell <superpuperrbernard@mail.ru>
2017-04-08 02:56:29 -0700

    Fix

--- Generators/Program.cs
+++ Generators/Program.cs
@@ -58,7 +58,7 @@ namespace Generators
     {
         public int Next { get; }
 
-        public ConstGenerator(int inputValue)
+        public ConstGenerator(int inputValue = 0)
         {
             Next = inputValue;
         }
